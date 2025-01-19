d4efcd8b4d31637ca2343eff3a8a78a5c07d9009
pfaffelh <pfaffelh@gmail.com>
2025-01-20 00:54:30 +0100

    added some demo

--- LeanDemo.lean
+++ LeanDemo.lean
@@ -19,35 +19,42 @@ lemma l1 (n : ℕ) : n ∣ n := by
 example (j k l : ℕ) (hjk : j ∣ k) (hkl : k ∣ l) : j ∣ l := by
   exact Nat.dvd_trans hjk hkl
 
-open Function Set
+open Finset Set MeasureTheory Order
 
-variable {α : Type} [PartialOrder α] [OrderBot α]
+example [PartialOrder α] [OrderBot α] (x y : α) (h : Disjoint x y) (he : x ≠ ⊥ ∨ y ≠ ⊥) : x ≠ y := by
+  exact (Disjoint.ne_iff h).mpr he
 
-example (J : Set α) (s t : α) (hs : ⋃₀ J ⊆ s) (ht : ⋃₀ K ⊆ t) (hd : Disjoint s t) : PairwiseDisjoint J id := by
+example {α : Type} [CompleteSemilatticeSup α] (K : Set α) (y : α) (hy : y ∈  K) : y ≤ sSup K := by
+  apply CompleteSemilatticeSup.le_sSup K y hy
 
-  exact hd hs ht hst
+
+
+lemma sSup_disjoint_of_le_of_le {α : Type*} [CompleteLattice α] (s t : α) (J K : Set α) (hs : ∀ b ∈ J, b ≤ s) (ht : ∀ b ∈ K, b ≤ t) (hd : Disjoint s t) (he : ⊥ ∉ J ∨ ⊥ ∉ K) : Disjoint J K := by
+  rw [disjoint_iff_forall_ne]
+  intros x hx y hy
+  rw [Disjoint.ne_iff]
+  aesop
+  exact Disjoint.mono (hs x hx) (ht y hy) hd
+
+lemma sSup_disjoint {α : Type*} [CompleteLattice α] (a b : Set α) (hd : Disjoint (sSup a) (sSup b)) (he : ⊥ ∉ a ∨ ⊥ ∉ b) : Disjoint a b := sSup_disjoint_of_le_of_le (sSup a) (sSup b) a b (fun _ hc => le_sSup hc) (fun _ hc => le_sSup hc) hd he
+
+
+
+
+example {α : Type*} (s t : Set α) (J K : Set (Set α)) (hs : ∀ b ∈ J, b ⊆ s) (ht : ∀ b ∈ K, b ⊆ t) (hd : Disjoint s t) (he : ∅ ∉ J ∨ ∅ ∉ K) : Disjoint J K := sSup_disjoint_of_le_of_le s t J K hs ht hd he
+
+
+theorem disjoint_sSup_left{α : Type u_1} [CompleteLattice α] {a : Set α} {b : α} (d : Disjoint (sSup a) b) {i : α} (hi : i ∈ a) :
+Disjoint i b
+
 
 lemma disjointSets_of_disjoint {α : Type*} [CompleteLattice α] {a b : α} {J K : Set α}
     (ha : ∀ c ∈ J, c ≤ a) (hb : ∀ d ∈ K, d ≤ b) (hJK : ⊥ ∉ J ∨ ⊥ ∉ K) (hcd : Disjoint a b) :
     Disjoint J K := by
+  exact disjointSets_of_disjoint' a b J K ha hb hcd hJK
 
 
-  rw [disjoint_of_pairwiseDisjoint]
-   PairwiseDisjoint {J, K}
-
-  simp
-  rw [Set.disjoint_iff_forall_ne]
-  intros x hx y hy hxy
-  obtain h1 : Disjoint x y := by
-    exact Disjoint.mono (ha x hx) (hb y hy) hcd
-  revert h1
-  cases' hJK with hJ hK
-  · rw [← hxy]
-    exact (not_iff_not.mpr disjoint_self).mpr (ne_of_mem_of_not_mem hx hJ)
-  · rw [hxy]
-    exact (not_iff_not.mpr disjoint_self).mpr (ne_of_mem_of_not_mem hy hK)
-
-lemma disjointSets_of_disjoint' {α : Type*} [PartialOrder α] [OrderBot α] {a b : α} {J K : Set α}
+lemma disjointSets_of_disjoint'' {α : Type*} [PartialOrder α] [OrderBot α] {a b : α} {J K : Set α}
     (ha : ∀ c ∈ J, c ≤ a) (hb : ∀ d ∈ K, d ≤ b) (hJK : ⊥ ∉ J ∨ ⊥ ∉ K) (hcd : Disjoint a b) :
     Disjoint J K := by
   rw [Set.disjoint_iff_forall_ne]
